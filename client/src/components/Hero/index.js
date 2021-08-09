afcd018b6bbb9a3fb37f455cd3b1a7c7ea843d89
Leon Hsu <leonhsu95@gmail.com>
2021-08-09 13:20:02 +1000

    Index Content Responsive

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -4,31 +4,63 @@ import HomePic from "../Photos/the-cold-drip1.png"
 
 function Hero(){
     return (
-        <div id="hero">
-            <div className="hero-left">
-                <h1 id="heading">
-                    The Cold Drip
-                </h1>
-                <p id="subtitle">
-                    The celebration of deliciousness. <br></br>
-                    The best of coffee culture brought together in one space.
-                </p>
-                <div className="line">
-                    <p className="scroll-text">Scroll Down to Discover</p>
+    <>
+        <div className="desktop">
+            <div id="hero">
+                <div className="hero-left">
+                    <h1 id="heading">
+                        The Cold Drip
+                    </h1>
+                    <p id="subtitle">
+                        The celebration of deliciousness. <br></br>
+                        The best of coffee culture brought together in one space.
+                    </p>
+                    <div className="line">
+                        <p className="scroll-text">Scroll Down to Discover</p>
+                    </div>
                 </div>
-            </div>
-            
-            <div className="hero-picture">
-                <img src={HomePic} alt="The Cold Drip"></img>
-            </div>
-            <div className="hero-right">
-                <video id="video-bg" autoPlay loop muted>
-                    <source src={Coffee} type="video/mp4">
-                    </source>
-                </video>
-            </div>
-            
-        </div>        
+
+                <div className="hero-picture">
+                    <img src={HomePic} alt="The Cold Drip"></img>
+                </div>
+                <div className="hero-right">
+                    <video id="video-bg" autoPlay loop muted>
+                        <source src={Coffee} type="video/mp4">
+                        </source>
+                    </video>
+                </div>
+
+            </div>    
+        </div>
+        <div className="responsive">
+            <div id="hero" className="responsive">
+                {/* <div className="hero-left">
+                    <h1 id="heading">
+                        The Cold Drip
+                    </h1>
+                    <p id="subtitle">
+                        The celebration of deliciousness. <br></br>
+                        The best of coffee culture brought together in one space.
+                    </p>
+                </div> */}
+
+                <div className="hero-picture">
+                    <h1 id="heading">
+                        The Cold Drip
+                    </h1>
+                    <img src={HomePic} alt="The Cold Drip"></img>
+                </div>
+
+                <div className="hero-right">
+                    <video id="video-bg" autoPlay loop muted>
+                        <source src={Coffee} type="video/mp4">
+                        </source>
+                    </video>
+                </div>
+
+            </div>      
+        </div>
+    </>
     )
 }
 
--- client/src/components/Supplies/index.js
+++ client/src/components/Supplies/index.js
@@ -7,7 +7,9 @@ const ssclass = `supplies-section-container wrapper`;
 
 function Supplies(){
     return (
-        <div id="supplies-section">
+
+    <>
+       <div id="supplies-section" className="desktop">
        <div className={ssclass}> 
             <div className="supplies-section-picture">
                 <img src={Jug} alt="Hario Cold Brew Jug"></img>
@@ -22,7 +24,25 @@ function Supplies(){
             </div>
         </div>
             
-        </div>          
+        </div>    
+
+        <div id="supplies-section" className="responsive">
+       <div className={ssclass}> 
+            <div className="supplies-section-text"> 
+                <h2 className="wrapper">Our Supplies</h2>
+                <p className="wrapper">We have a selection of World Class brands <br></br>that will help you make the best cup of coffee <br></br> of all style and types</p>
+                <div className="supplies-section-picture">
+                <img src={Jug} alt="Hario Cold Brew Jug"></img>
+                </div>
+                <Link id="supplies-section-button" className="buttons-index" to="/shop">
+                    View Selection
+                </Link>
+            </div>
+        </div>
+            
+        </div>   
+
+    </>      
     )
 }
 
